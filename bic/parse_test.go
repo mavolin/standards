36714c9e39d7cf2d6d0b4db8f5c87033c96a431d
mavolin <github@mavolin.co>
2023-01-22 13:03:55 +0100

    test(bic): add tests for spaces

--- bic/parse_test.go
+++ bic/parse_test.go
@@ -2,17 +2,35 @@ package bic
 
 import "testing"
 
-func TestValidate(t *testing.T) {
-	testCases := []string{
-		"BELADEBEXXX", "RBOSGGSX", "CHASGB2LXXX", "RZTIAT22263", "BCEELULL",
-		"MARKDEFF", "GENODEF1JEV", "UBSWCHZH80A", "CEDELULLXXX", "HELADEF1RRS",
-		"GENODEF1S04",
+func TestParse(t *testing.T) {
+	testCases := []struct {
+		In     string
+		Expect string
+	}{
+		{In: "BELADEBEXXX", Expect: "BELADEBEXXX"},
+		{In: "RBOSGGSX", Expect: "RBOSGGSX"},
+		{In: "CHASGB2LXXX", Expect: "CHASGB2LXXX"},
+		{In: "RZTIAT22263", Expect: "RZTIAT22263"},
+		{In: "BCEELULL", Expect: "BCEELULL"},
+		{In: "MARKDEFF", Expect: "MARKDEFF"},
+		{In: "GENODEF1JEV", Expect: "GENODEF1JEV"},
+		{In: "UBSWCHZH80A", Expect: "UBSWCHZH80A"},
+		{In: "CEDELULLXXX", Expect: "CEDELULLXXX"},
+		{In: "HELADEF1RRS", Expect: "HELADEF1RRS"},
+		{In: "GENODEF1S04", Expect: "GENODEF1S04"},
+		{In: "BELA DEBE XXX", Expect: "BELADEBEXXX"},
+		{In: "BELA    DEBE   XXX", Expect: "BELADEBEXXX"},
 	}
 
 	for _, c := range testCases {
-		t.Run(c, func(t *testing.T) {
-			if !IsValid(c) {
-				t.Errorf("expected %q to be valid", c)
+		t.Run(c.In, func(t *testing.T) {
+			bic, err := Parse(c.In)
+			if err != nil {
+				t.Errorf("Parse(%q): %s", c.In, err)
+			}
+
+			if bic.String() != c.Expect {
+				t.Errorf("Parse(%q): expected %q, got %q", c.In, c.Expect, bic.String())
 			}
 		})
 	}
